a199325c1d992332cfc264d16087dd4c7197e30e
poyrazavsever <poyrazavsever@hotmail.com>
2025-04-08 01:42:36 +0300

    feat: added animation with framer-motion and intersection-observer

--- components/InfoSection.tsx
+++ components/InfoSection.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import { FiPhone } from 'react-icons/fi'
+import { motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 
 const InfoSection = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 1,
+  })
+
+  const variants = {
+    hidden: { opacity: 0, y: -20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1, ease: 'easeOut' } },
+  }
+
   return (
-    <div className="w-full shadow-md py-8">
+    <motion.div
+      ref={ref}
+      variants={variants}
+      initial="hidden"
+      animate={inView ? 'visible' : 'hidden'}
+      className="w-full shadow-md py-8"
+    >
       <div className="container mx-auto max-w-7xl flex justify-between items-center">
         {/* Sol kısım: ikon + yazı */}
         <div className="flex items-center gap-4 text-bg font-semibold text-3xl">
@@ -19,7 +37,7 @@ const InfoSection = () => {
           +90 555 123 45 67
         </a>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
